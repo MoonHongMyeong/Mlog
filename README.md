cbf12cdacd6565e1e7ded3e022ed73426bd12f89
moon4368 <inv1064@gmail.com>
2021-01-18 01:29:47 +0900

    init project

---
+++ README.md
@@ -0,0 +1,17 @@
+# 포트폴리오용 블로그 토이 프로젝트
+
+프론트 작업은 react
+백엔드 작업은 springboot
+
+
+backend 
+java 11.0.9
+springboot 2.4.3(SNAPSHOT)
+
+spring data jpa
+spring devtools
+Lombok
+Junit5
+
+H2database dev only
+MySQL database 
---
+++ blog-backend/src/test/java/me/portpolio/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package me.portpolio.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
